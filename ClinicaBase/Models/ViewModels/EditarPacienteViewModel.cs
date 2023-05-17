4dd22c7b3da000d17ec1d0b66125efee523c77bc
BivianaMedina <biviana.medina.dev@gmail.com>
2023-05-17 13:36:52 -0500

    se agrega clase para modificar la información básica del paciente

---
+++ ClinicaBase/Models/ViewModels/EditarPacienteViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ClinicaBase.Models.ViewModels
+{
+    public class EditarPacieteViewModel
+    {
+        private const string RequiredError = "Campo obligatorio";
+
+        [StringLength(255)]
+        public string? Correo { get; set; } = null!;
+
+        [Required(ErrorMessage = RequiredError)]
+        [StringLength(150)]
+        public string Direccion { get; set; } = null!;
+
+        [Required(ErrorMessage = RequiredError)]
+        [StringLength(20)]
+        public string Telefono { get; set; } = null!;
+
+        [Required(ErrorMessage = RequiredError)]
+        [StringLength(100)]
+        public string Profesion { get; set; } = null!;
+
+        [Required(ErrorMessage = RequiredError)]
+        [StringLength(100)]
+        public string Ocupacion { get; set; } = null!;
+
+        [Required(ErrorMessage = RequiredError)]
+        [StringLength(20)]
+        public string TelefonoFamiliar { get; set; } = null!;
+
+        [Required(ErrorMessage = RequiredError)]
+        [StringLength(100)]
+        public string NombreFamiliar { get; set; } = null!;
+
+        [Required(ErrorMessage = RequiredError)]
+        [StringLength(30)]
+        public string TipoEps { get; set; } = null!;
+
+        [Required(ErrorMessage = RequiredError)]
+        [StringLength(50)]
+        public string NombreEps { get; set; } = null!;
+
+        [Required(ErrorMessage = RequiredError)]
+        public string ExamenFisico { get; set; } = null!;
+
+        [Required(ErrorMessage = RequiredError)]
+        public string Antecedentes { get; set; } = null!;
+
+        [Required(ErrorMessage = RequiredError)]
+        public string? AntecedentesFarmac { get; set; }
+    }
+}
