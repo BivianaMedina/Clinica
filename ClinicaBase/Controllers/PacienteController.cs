2a871ecae429751cf7a0b19e444241f0a5c8263d
BiviMed <bivimed99@gmail.com>
2023-05-19 04:49:03 -0500

    Permite guardar y modificar la información básica del paciente teniendo en cuenta el rol del usuario autenticado

--- ClinicaBase/Controllers/PacienteController.cs
+++ ClinicaBase/Controllers/PacienteController.cs
@@ -1,10 +1,14 @@
-﻿using ClinicaBase.Data;
+﻿using AutoMapper;
+using Azure.Core;
+using ClinicaBase.Data;
+using ClinicaBase.Models.Entities;
 using ClinicaBase.Models.ViewModels;
 using ClinicaBase.Responses;
 using ClinicaBase.Services.ServicioPacientes;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
+using System.Xml;
 
 namespace ClinicaBase.Controllers
 {
@@ -57,15 +61,90 @@ namespace ClinicaBase.Controllers
             {                
                 return View(request);
             }
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Index", "Home", response);
         }
-
 
         [Authorize(Roles = "Admin, Enfermeria")]
         [HttpGet]
-        public IActionResult ActualizarInfo()
-        {
+        public IActionResult BuscarPaciente()
+        {            
             return View();
+        }
+
+        [Authorize(Roles = "Admin, Enfermeria")]
+        [HttpPost]
+        public async Task<IActionResult> BuscarPaciente(BuscarPacienteViewModel request)
+        {
+            if (request.Documento == null && request.Nombres == null && request.Apellidos == null)
+            {
+                request.Succeeded = 0;
+                request.Message = "Debe llenar por lo menos un campo para buscar al paciente.";
+                return View(request);
+            }
+
+            GeneralResponse response = await _servicioPaciente.BuscarPacientes(request);
+            if (response.Succeed == 0)
+            {
+                request.Succeeded = response.Succeed;
+                request.Message = response.Message;
+            }
+            else
+            {
+                request.Succeeded = response.Succeed;
+                request.Pacientes = (List<Patient>)response.Data!;
+            }
+            return View(request);
+        }
+
+
+        [Authorize(Roles = "Admin, Medico, Enfermeria")]
+        [HttpGet]
+        public async Task<IActionResult> ActualizarInfo(int documento)
+        {
+            GeneralResponse response = await _servicioPaciente.BuscarPaciente(documento);
+            if (response.Succeed == 0)
+            {
+                return RedirectToAction("BuscarPaciente", "Paciente");
+            }
+
+            Patient paciente = (Patient)response.Data!;
+
+            _servicioPaciente.MapearPacienteAEditarPaciente(paciente, out EditarPacieteViewModel editarPaciente);
+            
+            return View(editarPaciente);
+        }
+
+        [Authorize(Roles = "Admin, Medico, Enfermeria")]
+        [HttpPost]
+        public async Task<IActionResult> ActualizarInfo(EditarPacieteViewModel editarRequest)
+        {
+            GeneralResponse response = new();
+            response = await _servicioPaciente.BuscarPaciente(editarRequest.Documento);
+
+            if (response.Succeed == 0)
+            {
+                editarRequest.Succeeded = response.Succeed;
+                editarRequest.Message = response.Message;
+                return View(editarRequest);
+            }
+            Patient paciente = (Patient)response.Data!;
+            _servicioPaciente.MapearEditarPacienteAPaciente(paciente, editarRequest, out Patient pacienteActualizado);
+
+            if (pacienteActualizado == null)
+            {
+                editarRequest.Succeeded = 0;
+                editarRequest.Message = "Se ha generado un error inesperado";
+                return View(editarRequest);
+            }
+            response = await _servicioPaciente.ActializarInformacion(pacienteActualizado);
+
+            if (response.Succeed == 0)
+            {
+                editarRequest.Succeeded = response.Succeed;
+                editarRequest.Message = response.Message;
+                return View(editarRequest);
+            }
+            return RedirectToAction("Index", "Home", response);
         }
 
     }
---
+++ ClinicaBase/Models/ViewModels/BuscarPacienteViewModel.cs
@@ -0,0 +1,19 @@
+﻿using ClinicaBase.Models.Entities;
+
+namespace ClinicaBase.Models.ViewModels
+{
+    public class BuscarPacienteViewModel
+    {
+        public int? Documento { get; set; } = null;
+
+        public string? Nombres { get; set; } = null;
+
+        public string? Apellidos { get; set; } = null;
+
+        public int? Succeeded { get; set; } = null;
+
+        public string? Message { get; set; } = null;
+
+        public List<Patient>? Pacientes { get; set; } = null;
+    }
+}
